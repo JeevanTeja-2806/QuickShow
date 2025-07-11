691f2ba86c56610967eef8e0f0920a2555edadc0
Jeevan Teja <nasajeevan2806@gmail.com>
2025-07-11 15:10:58 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favourites from './pages/Favourites'
+import {Toaster} from 'react-hot-toast'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShow from './pages/admin/ListShow'
+import ListBookings from './pages/admin/ListBookings'
+import Layout from './pages/admin/Layout'
+import './index.css'; // or whatever your global file is called
+
+
+const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  return (
+    <>
+    <Toaster/>
+      {!isAdminRoute && <Navbar/>}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:id' element={<MovieDetails/>}/>
+        <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+        <Route path='/favourites' element={<Favourites/>}/> 
+        <Route path='/admin/*' element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path='add-shows' element={<AddShows/>}/>
+          <Route path='list-shows' element={<ListShow/>}/>
+          <Route path='list-bookings' element={<ListBookings/>}/>
+        </Route>
+      </Routes>
+      {!isAdminRoute && <Footer/>}
+    </>
+  )
+}
+
+export default App
