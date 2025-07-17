48bf101f4aef89b1d4894bf3db9998e9454dd6de
Jeevan Teja <nasajeevan2806@gmail.com>
2025-07-17 15:37:23 +0530

    function added to release seats after 10 minutes

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,7 +1,7 @@
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js"
 import stripe from 'stripe'
-import inngest  from "inngest";
+import {inngest}  from '../inngest/index.js';
 
 
 //Function to check availabitiy of selected seats for a movie
